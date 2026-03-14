cbee3e38ce36b1a96ed7620c0fa41c3cb7f012e9
CuocThien <thiencuoc2210@gmail.com>
2026-03-14 11:42:56 +0700

    feat(frontend): update entry point with all providers
    - Configure main.tsx with all providers (Config, i18n, Apollo)
    - Import from new app structure (@/app/App)
    - Add global styles import
    - Delete old App.tsx from src root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ConfigProvider } from 'antd';
+import { App } from '@/app/App';
+import { ApolloProviderWrapper } from '@/app/providers/ApolloProvider';
+import { I18nProvider } from '@/app/providers/I18nProvider';
+import '@/styles/global.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#0ea5e9',
+        },
+      }}
+    >
+      <I18nProvider>
+        <ApolloProviderWrapper>
+          <App />
+        </ApolloProviderWrapper>
+      </I18nProvider>
+    </ConfigProvider>
+  </React.StrictMode>
+);
